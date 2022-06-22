1f1f64ef66b5c6d71192f580655223dbe302cbb9
Thathsarani Nimsara Narasinghe Bandara <cisco1998learn@gmail.com>
2022-06-21 22:58:34 -0700

    Strings basic

---
+++ string.js
@@ -0,0 +1,13 @@
+let myName="Thathsarani";
+
+const resultSlince=myName.slice(2,5);
+console.log(resultSlince);
+
+const resultSubstr=myName.substring(1,3);
+console.log(resultSubstr);
+
+const resultIndexof=myName.indexOf('i');
+console.log(resultIndexof);
+
+const resultLastIndexof=myName.lastIndexOf('n');
+console.log(resultLastIndexof);
