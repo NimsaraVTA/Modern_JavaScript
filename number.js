bf922e749826d0eaf3bc8aab0c74af6a5273e9ab
Thathsarani Nimsara Narasinghe Bandara <cisco1998learn@gmail.com>
2022-06-21 23:16:40 -0700

    Numbers basics

---
+++ number.js
@@ -0,0 +1,28 @@
+let num=20;
+
+let result1=(num+5);
+console.log(result1);
+
+let result2=(num-5);
+console.log(result2);
+
+let result3=(num*5);
+console.log(result3);
+
+let result4=(num/5);
+console.log(result4);
+
+let result5=(num**2);
+console.log(result5);
+
+let result6=(num++);
+console.log(result6);
+
+let result8=(num--);
+console.log(result8);
+
+let result9=(num+=5);
+console.log(result9);
+
+let result10=(num-=5);
+console.log(result10);
